f73d18c1ad24024c35a422862646175749f854b4
Breno Maia <brenomaia98@gmail.com>
2022-04-08 19:48:01 -0300

    Initial repo config (eslint, prettier, absolute imports, styled-components)

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles.Home';
+
+const Home: React.FC = () => {
+  return (
+    <Container>
+      <h1>Home Page</h1>
+    </Container>
+  );
+};
+
+export default Home;
