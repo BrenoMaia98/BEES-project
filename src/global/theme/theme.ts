4d4cc1ade6ba2ca40d22460fe59ed6640d932680
Breno Maia <brenomaia98@gmail.com>
2022-04-09 20:11:50 -0300

    adding more variety to palette

--- src/global/theme/theme.ts
+++ src/global/theme/theme.ts
@@ -1,6 +1,11 @@
 export type Palette = {
   primary: string;
+  primaryLight: string;
+  primaryDark: string;
   secondary: string;
+  secondaryLight: string;
+  secondaryDark: string;
+  background2: string;
   background: string;
   font: {
     primaryBg: string;
@@ -12,7 +17,12 @@ export type GlobalThemeType = { light: Palette; dark: Palette };
 const globalTheme: GlobalThemeType = {
   light: {
     primary: '#F3EC57',
+    primaryLight: '#ffff8a',
+    primaryDark: '#bdba1f',
     secondary: '#52515C',
+    secondaryLight: '#7e7d89',
+    secondaryDark: '#292933',
+    background2: '#FFF',
     background: '#FFFEF2',
     font: {
       primaryBg: '#00010D',
@@ -20,8 +30,13 @@ const globalTheme: GlobalThemeType = {
     },
   },
   dark: {
-    primary: '#52515C',
     secondary: '#F3EC57',
+    secondaryLight: '#ffff8a',
+    secondaryDark: '#bdba1f',
+    primary: '#52515C',
+    primaryLight: '#7e7d89',
+    primaryDark: '#292933',
+    background2: '#000',
     background: '#00010D',
     font: {
       primaryBg: '#fff',
