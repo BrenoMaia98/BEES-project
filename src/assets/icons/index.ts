56c830d56942ace44a3aff038b3a869d0fba9c6d
Breno Maia <brenomaia98@gmail.com>
2022-04-08 22:36:04 -0300

    Adding img and Icond default export

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,17 @@
 import { ReactComponent as WebIcon } from './web.svg';
+import { ReactComponent as ArrowBackIcon } from './arrow-back.svg';
+import { ReactComponent as GraphIcon } from './graph.svg';
+import { ReactComponent as LocationMarkerIcon } from './location-marker.svg';
+import { ReactComponent as PhoneIcon } from './phone.svg';
+import { ReactComponent as PlusOutlineIcon } from './plus-outline.svg';
+import { ReactComponent as TrashIcon } from './trash.svg';
 
-export { WebIcon };
+export {
+  WebIcon,
+  ArrowBackIcon,
+  GraphIcon,
+  LocationMarkerIcon,
+  PhoneIcon,
+  PlusOutlineIcon,
+  TrashIcon,
+};
