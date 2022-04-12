365461d101f163cd219f83e974cd42757a92cdd3
Breno Maia <brenomaia98@gmail.com>
2022-04-12 19:35:28 -0300

    fix api test cases, axios mock error

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
-export const api = axios.create({
-  timeout: 3000,
-});
 // Couldnt use inside create due to 'axios-mock-adapter' open issue with baseUrl config
 // https://github.com/ctimmerm/axios-mock-adapter/issues/299
+// needed to remove axios config
+export const api = axios;
 export const baseUrl = 'https://api.openbrewerydb.org/';
