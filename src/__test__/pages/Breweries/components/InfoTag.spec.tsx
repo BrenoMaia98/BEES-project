7339c161066a8e1509cc1330c299299158b91227
Breno Maia <brenomaia98@gmail.com>
2022-04-12 19:03:42 -0300

    adding InfoTag test case: InputText autofocus and hide

--- src/__test__/pages/Breweries/components/InfoTag.spec.tsx
+++ src/__test__/pages/Breweries/components/InfoTag.spec.tsx
@@ -1,28 +1,41 @@
 import React from 'react';
-import { fireEvent, render, RenderResult } from '@testing-library/react';
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  waitFor,
+} from '@testing-library/react';
 import { GlobalThemeProvider } from 'global/theme/globalThemeProvider';
 import { IconsDataTestIdEnum } from 'assets/icons';
 import {
   InfoTag,
   InfoTagProps,
 } from 'pages/Breweries/components/BreweryCard/InfoTag';
+import { debug } from 'console';
+
+const testIds = {
+  text: 'text-tag-info',
+  icon: IconsDataTestIdEnum.GraphIcon,
+  addIcon: IconsDataTestIdEnum.PlusOutlineIcon,
+  removeIcon: IconsDataTestIdEnum.TrashIcon,
+  defaultTag: 'default-tag',
+  addTag: 'add-tag',
+  removeableTag: 'removeable-tag',
+};
 
 const mockDefaultTag: InfoTagProps = {
   type: 'default',
   icon: 'GraphIcon',
   text: 'mockDefaultTag',
-  'data-testid': 'default-tag',
 };
 const mockAddInfoTag: InfoTagProps = {
   type: 'addInfo',
   text: 'mockAddInfoTag',
-  'data-testid': 'add-tag',
   action: jest.fn(),
 };
 const mockRemoveableTag: InfoTagProps = {
   type: 'removeable',
   text: 'mockRemoveableTag',
-  'data-testid': 'removeable-tag',
   action: jest.fn(),
 };
 
@@ -33,23 +46,35 @@ const renderComponent = (
   return render(
     <GlobalThemeProvider>
       {type === 'addInfo' && (
-        <InfoTag {...mockAddInfoTag} text={text || mockAddInfoTag.text} />
+        <InfoTag
+          {...mockAddInfoTag}
+          text={text || mockAddInfoTag.text}
+          data-testid={testIds.addTag}
+        />
       )}
       {type === 'default' && (
-        <InfoTag {...mockDefaultTag} text={text || mockDefaultTag.text} />
+        <InfoTag
+          {...mockDefaultTag}
+          text={text || mockDefaultTag.text}
+          data-testid={testIds.defaultTag}
+        />
       )}
       {type === 'removeable' && (
-        <InfoTag {...mockRemoveableTag} text={text || mockRemoveableTag.text} />
+        <InfoTag
+          {...mockRemoveableTag}
+          text={text || mockRemoveableTag.text}
+          data-testid={testIds.removeableTag}
+        />
       )}
     </GlobalThemeProvider>
   );
 };
 
-const testIds = {
-  text: 'text-tag-info',
-  icon: IconsDataTestIdEnum.GraphIcon,
-  addIcon: IconsDataTestIdEnum.PlusOutlineIcon,
-  removeIcon: IconsDataTestIdEnum.TrashIcon,
+const displayInputText = (component: RenderResult) => {
+  const infoAddTag = component.getByTestId(testIds.addTag);
+  expect(infoAddTag).not.toBeNull();
+
+  fireEvent.click(infoAddTag);
 };
 
 describe('InfoTag', () => {
@@ -86,18 +111,52 @@ describe('InfoTag', () => {
 
   describe('InfoTag type="addInfo"', () => {
     it('Should display InputText when clicked on InfoTag', () => {
-      const { queryByTestId } = renderComponent('addInfo');
-      const infoAddTag = queryByTestId(mockAddInfoTag['data-testid'] || '');
-
-      expect(infoAddTag).not.toBeNull();
-
-      if (infoAddTag) {
-        fireEvent.click(infoAddTag);
-      }
-
+      const component: RenderResult = renderComponent('addInfo');
+      const { queryByTestId } = component;
+      displayInputText(component);
       const inputText = queryByTestId('input-text');
 
       expect(inputText).not.toBeNull();
+    });
+
+    it('Should autoFocus InputText when displayed', async () => {
+      const component: RenderResult = renderComponent('addInfo');
+      const { queryByTestId } = component;
+      displayInputText(component);
+      const inputText = queryByTestId('input-text');
+
+      expect(document.activeElement === inputText).toBeTruthy();
+      // await waitFor(() => expect(inputText).toHaveFocus());
+    });
+
+    it('Should hide InputText on "Enter" press', async () => {
+      const component: RenderResult = renderComponent('addInfo');
+      const { getByTestId, queryByTestId } = component;
+      displayInputText(component);
+      const inputText = getByTestId('input-text');
+
+      fireEvent.keyDown(inputText, { key: 'Enter', charCode: 13 });
+
+      expect(queryByTestId('input-text')).toBeNull();
+    });
+
+    it('Should hide InputText onBlur', () => {
+      const component: RenderResult = renderComponent('addInfo');
+      const { getByTestId, queryByTestId } = component;
+      displayInputText(component);
+      const inputText = getByTestId('input-text');
+
+      fireEvent.blur(inputText);
+
+      expect(queryByTestId('input-text')).toBeNull();
+    });
+
+    it('Should call InfoTag action props on InputText Blur', () => {
+      expect(true).toBeTruthy();
+    });
+
+    it('Should call InfoTag action props on "Enter" press', () => {
+      expect(true).toBeTruthy();
     });
   });
 });
