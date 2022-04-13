31af5d745eb803a217c57d3461dbfa66f3293621
Breno Maia <brenomaia98@gmail.com>
2022-04-12 22:44:24 -0300

    Readme asset

--- src/assets/imgs/index.ts
+++ src/assets/imgs/index.ts
@@ -1,6 +1,5 @@
 import BeeSrcImg from './bee.png';
 import WelcomeLogoBlack from './welcomeLogo-black.png';
 import WelcomeLogoWhite from './welcomeLogo-white.png';
-import BeesLogo from './beesLogo.png';
 
-export { BeeSrcImg, WelcomeLogoBlack, WelcomeLogoWhite, BeesLogo };
+export { BeeSrcImg, WelcomeLogoBlack, WelcomeLogoWhite };
